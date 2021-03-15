a412fdc1a86da20b49e2256526d27dd15e4a74e2
alexandru <boncut.alex10@gmail.com>
2021-03-14 23:05:25 -0500

    commit tree f02175c50308b57d629408724232f42e05f3a999

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+
+class School
+    attr_accessor :school_name, :roster
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = Hash.new
+    end
+
+    def add_student(name,grade)
+        if roster[grade]
+          roster[grade] << name
+        else
+          roster[grade] = []
+          roster[grade] << name
+        end
+    end
+
+    def grade(grade)
+        @roster.fetch(grade)
+    end
+
+    def sort
+        sorted_hash = {}
+        @roster.sort.each do |grade, name_array|
+          sorted_hash[grade] = (name_array.sort)
+        end
+        sorted_hash
+    end
+
+end
